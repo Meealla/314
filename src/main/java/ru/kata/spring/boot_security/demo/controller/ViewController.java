e15fdbc80d52bfc4184503b4976ab518ecba2ec2
Meealla <achvokina@mail.ru>
2025-01-01 23:46:18 +0300

    314

--- src/main/java/ru/kata/spring/boot_security/demo/controller/ViewController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/ViewController.java
@@ -1,15 +1,16 @@
-//package ru.kata.spring.boot_security.demo.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class ViewController {
-//
-//    // Главная страница администратора
-//    @GetMapping("/admin")
-//    public String adminPage() {
-//        return "admin"; // Возвращает HTML-шаблон admin.html
-//    }
-//
-//}
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ViewController {
+
+    /* Главная страница администратора */
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin"; /* Возвращает HTML-шаблон admin.html */
+    }
+}
